6220fd8b949689a21d356693fc9c9b9adfb50f0b
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-04-11 21:19:56 +0800

    Add Wuhan level

---
+++ Assets/Scripts/Wuhan/ProteaseSpawnerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProteaseSpawnerControl : MonoBehaviour
+{
+    [SerializeField] private float RespawnInterval = 2f;
+    [SerializeField] private float ProteaseSpeed = 10;
+    [SerializeField] private GameObject ProteasePrefab;
+    private Vector2[] Vertexes;
+    private float LastRespawnTime;
+    void Start()
+    {
+        Vertexes = new Vector2[transform.childCount];
+        for (int i = 0; i < transform.childCount; ++i)
+            Vertexes[i] = transform.GetChild(i).position;
+        Respawn();
+    }
+
+    void Update()
+    {
+        if (Time.time - LastRespawnTime >= RespawnInterval)
+            Respawn();
+    }
+
+    private void Respawn()
+    {
+        ProteaseControl NewProteaseControl = Instantiate<GameObject>(ProteasePrefab).GetComponent<ProteaseControl>();
+        NewProteaseControl.SetSpeed(ProteaseSpeed);
+        NewProteaseControl.SetVertexes(Vertexes);
+        NewProteaseControl.transform.position = transform.position;
+        LastRespawnTime = Time.time;
+    }
+}
