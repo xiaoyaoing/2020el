ba3be4a09c529b839a1e677668f02996f477a6e9
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-04-15 21:52:39 +0800

    complete zm's art

--- Assets/Scripts/Map/RegionControl.cs
+++ Assets/Scripts/Map/RegionControl.cs
@@ -4,6 +4,7 @@ public class RegionControl : MonoBehaviour
 {
     private string CurrentRegion;
     private SpriteRenderer Renderer;
+    private float FadeinTime = 3f;
 
     void Start()
     {
@@ -15,7 +16,12 @@ public class RegionControl : MonoBehaviour
 
     void Update()
     {
-        if (GameStatus.GetRegionInfectionStatus(CurrentRegion))
+        if (GameStatus.GetRegionInfectionStatus(CurrentRegion) && Renderer.enabled == false)
+        {
             Renderer.enabled = true;
+            Renderer.color = new Color(Renderer.color.r, Renderer.color.g, Renderer.color.b, 0);
+        }
+        if (Renderer.color.a < 1)
+            Renderer.color = new Color(Renderer.color.r, Renderer.color.g, Renderer.color.b, Renderer.color.a + 1 * Time.deltaTime / FadeinTime);
     }
 }
--- Assets/Scripts/Xi'an/RedcellControl.cs
+++ Assets/Scripts/Xi'an/RedcellControl.cs
@@ -3,27 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RedcellControl : MonoBehaviour
-
 {
-    private Transform PlayerTr, RedcellTr;
-    private Rigidbody2D RedcellRb;
-    private int speed = 5;
-    private bool label = false;
+    public float rotateSpeed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        PlayerTr = GameObject.Find("Player").transform;
-        RedcellTr = transform;
-        RedcellRb = GetComponent<Rigidbody2D>();
+        transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (RedcellTr.position.x > PlayerTr.position.x + 10)
-            label = true;
-        if (label)
-            RedcellRb.velocity = new Vector2(-speed, 0);
-
+        transform.Rotate(new Vector3(0, 0, 1) * rotateSpeed);
     }
 }
