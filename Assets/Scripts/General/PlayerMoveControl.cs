462ba0847102ae44e8b74bce381aaf4a9b9a6ba3
1924188282@qq.com <1924188282@qq.com>
2020-04-19 16:09:46 +0800

    gz

--- Assets/Scripts/General/PlayerMoveControl.cs
+++ Assets/Scripts/General/PlayerMoveControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerMoveControl : MonoBehaviour
 {
@@ -16,16 +17,21 @@ public class PlayerMoveControl : MonoBehaviour
 
     private bool IsInfecting;
     private Transform InfectCellPos;
+    Scene scene;
+    public int num_of_normal_cell,time,t1,t2=0 ;
 
     void Start()
     {
         JumpBox = GetComponent<BoxCollider2D>();
         PlayerRb = GetComponent<Rigidbody2D>();
         GroundMask = LayerMask.GetMask("Ground");
+        scene=SceneManager.GetActiveScene();
     }
 
     void Update()
-    {
+    {   
+        if(scene.name=="GuangZhou")
+        move_in_guangzhou();
         Move();
     }
     void Move()
@@ -58,7 +64,28 @@ public class PlayerMoveControl : MonoBehaviour
     {
         if (other.tag == "white cell")
             Destroy(gameObject);
-
+        if(other.tag == "normalcell")
+        {   Destroy(other.gameObject);
+            num_of_normal_cell++;}
+        if(other.tag=="target")
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if(other.tag=="deadline")
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void move_in_guangzhou(){
+        time++;
+        if(Input.GetKey(KeyCode.A) && num_of_normal_cell>=2 && time-t1>=30)
+        {transform.position=new Vector3(transform.position.x+5,transform.position.y,transform.position.y);
+         num_of_normal_cell-=2;
+        t1=time;
+        }
+        if(Input.GetKey(KeyCode.S) && num_of_normal_cell>=1 && time-t2>=30){
+            MaxHorizontalSpeed=(float)20;
+            num_of_normal_cell-=1;
+            t2=time;
+        }
+        if(time-t2==30)
+        MaxHorizontalSpeed=(float)10;
     }
 
     public void InfectCell(Transform pos)
