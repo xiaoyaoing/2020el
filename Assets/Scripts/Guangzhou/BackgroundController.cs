f2b632fa009369181368b21ec9b9619ccf592167
MOLGYPETER <1186786523@qq.com>
2020-05-02 01:00:28 +0800

    1

---
+++ Assets/Scripts/Guangzhou/BackgroundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer sr1, sr2;
+    int dir = -1;
+
+    void Update()
+    {
+        float alpha = sr1.color.a + 0.001f * dir;
+        if (alpha >= 1) dir = -1;
+        if (alpha <= 0) dir = 1;
+        sr1.color = new Color(1, 1, 1, alpha);
+        sr2.color = new Color(1, 1, 1, 1 - alpha);
+    }
+}
