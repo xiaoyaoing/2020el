fa0a30eec3891e0a679818390e2acd5fb9af645c
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-05-04 12:49:05 +0800

    make virus running after rushing

--- Assets/Scripts/GZ/PlayerRushControl.cs
+++ Assets/Scripts/GZ/PlayerRushControl.cs
@@ -6,11 +6,19 @@ public class PlayerRushControl : MonoBehaviour
 {
     private int RNACount;
 
+    Rigidbody2D PlayerRb;
+
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.A) && RNACount >= 2)
         {
             transform.position = new Vector3(transform.position.x + 5, transform.position.y, transform.position.y);
+            PlayerRb.velocity = new Vector2(10, PlayerRb.velocity.y);
             RNACount -= 2;
         }
     }
