342b54a949ae3d1b4dbcffd1b47ea66edd37b3c3
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-05-09 23:11:49 +0800

    Add hello scene

---
+++ Assets/Scripts/HelloScene/DotControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DotControl : MonoBehaviour
+{
+    private float ChangeInterval;
+    private bool Increasing = true;
+    private Image DotImage;
+
+    private static System.Random random = new System.Random();
+
+    void Start()
+    {
+        ChangeInterval = (float)(1 + random.NextDouble());
+        DotImage = GetComponent<Image>();
+    }
+
+    void FixedUpdate()
+    {
+        float TargetAlpha = DotImage.color.a + (Increasing ? 1 : -1) * Time.fixedDeltaTime / ChangeInterval;
+        if (TargetAlpha >= 1)
+        {
+            TargetAlpha = 1;
+            Increasing = false;
+        }
+        else if (TargetAlpha <= 0)
+        {
+            TargetAlpha = 0;
+            Increasing = true;
+        }
+        DotImage.color = new Color(DotImage.color.r, DotImage.color.g, DotImage.color.b, TargetAlpha);
+    }
+}
---
+++ Assets/Scripts/HelloScene/HelloSceneControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloSceneControl : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            UnityEngine.SceneManagement.SceneManager.LoadScene("WorldMap");
+    }
+}
