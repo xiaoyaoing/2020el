8402f0bc8f2d81bff3eef58f5ea3dc0a2beb4764
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-05-10 18:05:06 +0800

    Add credit scene

---
+++ Assets/Scripts/HelloScene/CurtainControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurtainControl : MonoBehaviour
+{
+    private RawImage Curtain;
+    private float FadeoutTime = 1.5f;
+
+    void Start()
+    {
+        Curtain = GetComponent<RawImage>();       
+    }
+
+    void Update()
+    {
+        Curtain.color = new Color(Curtain.color.r, Curtain.color.g, Curtain.color.b, Math.Max(0f, Curtain.color.a - Time.deltaTime / FadeoutTime));   
+    }
+}
